319fb9e511159e820a39b532f5eb2cb9038d9f89
Phi Mai <dinhphi2205@gmail.com>
2024-01-21 15:24:12 +0700

    add splash screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {ListScreen} from '../screens/ListScreen';
 import {WorkflowScreen} from '../screens/WorkflowScreen';
 import {WorkFlow} from '../utils/types';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
 export const AppStack = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
